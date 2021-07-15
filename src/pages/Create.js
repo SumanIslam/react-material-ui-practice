fefa06f38180a7d1b113d183264b227fae800e01
SumanIslam <sumanislam@gmail.com>
2021-07-15 09:17:55 +0600

    practice material ui button and button-group

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,18 +1,41 @@
 import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import React from 'react';
+import { Container } from '@material-ui/core';
 
 export default function Create() {
   return (
-    <div>
+    <Container>
+      {/* typography practice goes here*/}
       <Typography 
-        variant="h1"
-        component="h2"
+        variant="h5"
         color="textSecondary"
-        align="center"
         gutterBottom
       >
         Create a new note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log('you clicked me')}
+        color="secondary" 
+        variant="contained" 
+        type="submit"
+      >
+        Submit
+      </Button>
+
+      {/* buttons practice goes here
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color='primary'>Primary</Button>
+      <Button variant="contained" color = 'secondary'>secondary</Button>
+      <Button color="primary">submit</Button>
+      <Button color="secondary" variant="outlined">submit</Button>
+
+      <ButtonGroup color="primary" variant="text">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
